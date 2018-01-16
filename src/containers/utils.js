fc61f35562cf2f74417940ef657d06444b413a6d
Loi Tran <lvt15@my.fsu.edu>
2018-01-16 15:12:52 +0700

    Removed unused libraries

--- src/containers/utils.js
+++ src/containers/utils.js
@@ -1,20 +1,3 @@
-import { tsvParse } from  "d3-dsv";
-import { timeParse } from "d3-time-format";
-
-function parseData(parse) {
-	return function(d) {
-		d.date = new Date(d.date);
-		d.open = d.open;
-		d.high = d.high;
-		d.low = d.low;
-		d.close = d.close;
-
-		return d;
-	};
-}
-
-const parseDate = timeParse("%Y-%m-%d");
-
 export function getData() {
 	const promiseMSFT = fetch("http://localhost:3000")
 	.then((response) => response.json())
